5f119070738bacbd8f98f4e49d2b63bb3de53b53
Maximilian Kromer <a.makro@web.de>
2019-11-02 18:25:14 +0100

    test coords

--- js/main.js
+++ js/main.js
@@ -1,72 +1,35 @@
-function starttest() {
-    a = new Date();
-    b = a.getHours();
-    c = a.getMinutes();
-    d = a.getSeconds();
-    zeit = b+':'+c+':'+d;
+function showError(error) {
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            console.log("Benutzer lehnte Standortabfrage ab.");
+            break;
+        case error.POSITION_UNAVAILABLE:
+            console.log("Standortdaten sind nicht verfügbar.");
+            break;
+        case error.TIMEOUT:
+            console.log("Die Standortabfrage dauerte zu lange (Time-out).");
+            break;
+        case error.UNKNOWN_ERROR:
+            console.log("unbekannter Fehler.");
+            break;
+    }
+}
 
-    var ausgabe = document.getElementById('ausgabe');
+function start() {
+
+    var ausgabe2 = document.getElementById('ausgabe');
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(evCoords, evError, {
+        navigator.geolocation.watchPosition(evCoords, showError, {
             enableHighAccuracy: true, 
             timeout: 5000, 
             maximumAge: 5000
          });
     } else { 
-        ausgabe.innerHTML = 'Ihr Browser unterstützt keine Geolocation.';
+        alert('Ihr Browser unterstützt keine Geolocation.');
     }
 }
 
 function evCoords(position) {
-    a1 = new Date(position.timestamp);
-    b1 = a1.getHours();
-    c1 = a1.getMinutes();
-    d1 = a1.getSeconds();
-    zeit1 = b1+':'+c1+':'+d1;
-    ausgabe.innerHTML = "Test Erfolgreich:<br>" 
-        
-        + "<br>Endzeit: " + zeit1
-        + "<br>Breite: " + position.coords.latitude 
-        + "<br>Länge: " + position.coords.longitude
-        + "<br>Genauigkeit: " + position.coords.accuracy
-        + "<br>Höhe: " + position.coords.altitude
-        + "<br>Höhengenauigkeit: " + position.coords.altitudeAccuracy
-        + "<br>Richtung: " + position.coords.heading
-        + "<br>Geschwindigkeit: " + position.coords.speed;
-}
-
-function evError(error) {
-    switch(error.code) {
-        case error.PERMISSION_DENIED:
-            ausgabe.innerHTML = "Benutzer lehnte Standortabfrage ab."
-            break;
-        case error.POSITION_UNAVAILABLE:
-            ausgabe.innerHTML = "Standortdaten sind nicht verfügbar."
-            break;
-        case error.TIMEOUT:
-            ausgabe.innerHTML = "Die Standortabfrage dauerte zu lange (Time-out)."
-            break;
-        case error.UNKNOWN_ERROR:
-            ausgabe.innerHTML = "unbekannter Fehler."
-            break;
-    }
-}
-
-function starttest2() {
-
-    var ausgabe2 = document.getElementById('ausgabe2');
-    if (navigator.geolocation) {
-        navigator.geolocation.watchPosition(evCoords2, evError, {
-            enableHighAccuracy: true, 
-            timeout: 5000, 
-            maximumAge: 5000
-         });
-    } else { 
-        ausgabe2.innerHTML = 'Ihr Browser unterstützt keine Geolocation.';
-    }
-}
-
-function evCoords2(position) {
     a1 = new Date(position.timestamp);
     b1 = a1.getHours();
     c1 = a1.getMinutes();
