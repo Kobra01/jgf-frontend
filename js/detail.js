5989b16de01c3c43b21405218327c804873632a0
Maximilian Kromer <a.makro@web.de>
2019-11-14 18:16:21 +0100

    fetching data

--- js/detail.js
+++ js/detail.js
@@ -13,6 +13,33 @@ function getUrlValues (querystring) {
     }
 }
 
+function fetchDetails(obj_id) {
+
+    const data = {
+        object_id: obj_id
+    };
+      
+    fetch(url, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        redirect: 'follow', // manual, *follow, error
+        referrer: 'no-referrer', // no-referrer, *client
+        body: JSON.stringify(data), // data can be `string` or {object}
+        headers:{
+            'Content-Type': 'application/json'
+        }
+    }).then(res => res.json())
+    .then(response =>  {
+        // objects = response.objects;
+        console.log('Success:', JSON.stringify(response));
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
 
 
-  var urlValues = new getUrlValues(location.search);
+var url = "https://www.mks-software.de/jgf/beta/get_details.php";
+var urlValues = new getUrlValues(location.search);
